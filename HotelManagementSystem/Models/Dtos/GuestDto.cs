c973cc529b7c5c4523a15a8dd8808f5327583c51
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-20 21:14:02 +0200

    Added dto for the guest entity

---
+++ HotelManagementSystem/Models/Dtos/GuestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelManagementSystem.Models.Dtos
+{
+    public class GuestDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNr { get; set; }
+    }
+}
