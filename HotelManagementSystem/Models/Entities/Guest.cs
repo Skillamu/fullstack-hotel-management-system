5d40938a58e4baa2018ae14ea07664ece140ff94
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-20 21:40:54 +0200

    Made properties read only and added constructor with parameters for safety and encapsulation purposes

---
+++ HotelManagementSystem/Models/Entities/Guest.cs
@@ -0,0 +1,18 @@
+﻿namespace HotelManagementSystem.Models.Entities
+{
+    public class Guest
+    {
+        public Guid Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string PhoneNr { get; }
+
+        public Guest (Guid id, string firstName, string lastName, string phoneNr)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNr = phoneNr;
+        }
+    }
+}
