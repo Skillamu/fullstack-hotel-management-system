f83a5a45563c5150c0ff9adbe1ee54013857eb9d
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-21 10:43:44 +0200

    Added room repository

---
+++ HotelManagementSystem/Repositories/IRoomRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelManagementSystem.Repositories
+{
+    public interface IRoomRepository
+    {
+    }
+}
---
+++ HotelManagementSystem/Repositories/RoomRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace HotelManagementSystem.Repositories
+{
+    public class RoomRepository : IRoomRepository
+    {
+    }
+}
