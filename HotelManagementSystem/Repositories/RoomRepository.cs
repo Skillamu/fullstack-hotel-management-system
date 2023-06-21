cf021af276402e279f4f5f3623591647f75e6e85
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-21 10:49:11 +0200

    Added private readonly field for the database connection instance coming through constructor injection

--- HotelManagementSystem/Repositories/RoomRepository.cs
+++ HotelManagementSystem/Repositories/RoomRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace HotelManagementSystem.Repositories
+﻿using System.Data.SqlClient;
+
+namespace HotelManagementSystem.Repositories
 {
     public class RoomRepository : IRoomRepository
     {
+        private readonly SqlConnection _conn;
+
+        public RoomRepository(SqlConnection conn)
+        {
+            _conn = conn;
+        }
     }
 }
