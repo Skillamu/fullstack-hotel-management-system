24a4b39ccea8d36248fbef905ed53627e85ae71f
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-26 14:14:20 +0200

    Add members in the reservation service

--- HotelManagementSystem.Core/ApplicationServices/ReservationService.cs
+++ HotelManagementSystem.Core/ApplicationServices/ReservationService.cs
@@ -1,6 +1,28 @@
-﻿namespace HotelManagementSystem.Core.ApplicationServices
+﻿using HotelManagementSystem.Core.DomainModel;
+using HotelManagementSystem.Core.DomainServices;
+
+namespace HotelManagementSystem.Core.ApplicationServices
 {
     internal class ReservationService
     {
+        private readonly IReservationRepository _reservationRepository;
+        private readonly IGuestRepository _guestRepository;
+
+        public ReservationService(IReservationRepository reservationRepository, IGuestRepository guestRepository)
+        {
+            _reservationRepository = reservationRepository;
+            _guestRepository = guestRepository;
+        }
+
+        public Reservation GetReservationById(Guid id)
+        {
+            return _reservationRepository.GetById(id);
+        }
+
+        public void Create(Reservation reservation)
+        {
+            _reservationRepository.Create(reservation);
+            _guestRepository.Create(reservation.Guest);
+        }
     }
 }
