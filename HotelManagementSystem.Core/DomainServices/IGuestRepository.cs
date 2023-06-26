e5583a218e326f058a24494140c524cfa0f57445
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-26 13:17:44 +0200

    Create repository for guest in domain services (looks like it's needed after all)

---
+++ HotelManagementSystem.Core/DomainServices/IGuestRepository.cs
@@ -0,0 +1,10 @@
+﻿using HotelManagementSystem.Core.DomainModel;
+
+namespace HotelManagementSystem.Core.DomainServices
+{
+    internal interface IGuestRepository
+    {
+        Guest GetById(Guid id);
+        void Create(Guest guest);
+    }
+}
