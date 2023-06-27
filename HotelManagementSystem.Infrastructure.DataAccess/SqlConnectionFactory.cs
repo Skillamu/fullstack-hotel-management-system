15776df0113bc9c062703df83a398554ea7e787d
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-27 14:18:33 +0200

    Create members for the sql connection factory

--- HotelManagementSystem.Infrastructure.DataAccess/SqlConnectionFactory.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/SqlConnectionFactory.cs
@@ -1,6 +1,19 @@
-﻿namespace HotelManagementSystem.Infrastructure.DataAccess
+﻿using System.Data.SqlClient;
+
+namespace HotelManagementSystem.Infrastructure.DataAccess
 {
     internal class SqlConnectionFactory
     {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public SqlConnection GetSqlConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
