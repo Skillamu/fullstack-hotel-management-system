c4d6cd3343628e820c7347781464d12e61ce6377
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-06-28 17:36:12 +0200

    Create additional members in the reservation DTO that represent the related data of the foreign keys in the reservation entity within the database

--- HotelManagementSystem.Infrastructure.DataAccess/ReservationDto.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/ReservationDto.cs
@@ -5,5 +5,9 @@
         public Guid Id { get; set; }
         public Guid GuestId { get; set; }
         public Guid RoomId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNr { get; set; }
+        public short RoomNr { get; set; }
     }
 }
