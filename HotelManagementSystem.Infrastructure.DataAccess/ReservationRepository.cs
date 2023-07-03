2285c2d1177348df96263ce20b752a1818bf5346
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-03 16:26:43 +0200

    Implement the IReservationRepository in ReservationRepository

--- HotelManagementSystem.Infrastructure.DataAccess/ReservationRepository.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/ReservationRepository.cs
@@ -1,12 +1,31 @@
-﻿namespace HotelManagementSystem.Infrastructure.DataAccess
+﻿using Dapper;
+using HotelManagementSystem.Core.DomainModel;
+using HotelManagementSystem.Core.DomainServices;
+
+namespace HotelManagementSystem.Infrastructure.DataAccess
 {
-    public class ReservationRepository
+    public class ReservationRepository : IReservationRepository
     {
         private readonly SqlConnectionFactory _sqlConnectionFactory;
 
         public ReservationRepository(SqlConnectionFactory sqlConnectionFactory)
         {
             _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public void Create(Reservation reservation)
+        {
+            var sql = @"INSERT INTO reservation VALUES (@Id, @GuestId, @RoomId)";
+
+            var parameters = new
+            {
+                Id = reservation.Id,
+                GuestId = reservation.Guest.Id,
+                RoomId = reservation.Room.Id
+            };
+
+            using var conn = _sqlConnectionFactory.CreateSqlConnection();
+            conn.Execute(sql, parameters);
         }
     }
 }
