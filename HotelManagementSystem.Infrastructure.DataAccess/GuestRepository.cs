2f5c31ce3de1a0b939bee78ffe7d2963ef44147f
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-03 16:02:32 +0200

    Add field for the SqlConnectionFactory instance obtained from a singleton service

--- HotelManagementSystem.Infrastructure.DataAccess/GuestRepository.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/GuestRepository.cs
@@ -2,5 +2,11 @@
 {
     public class GuestRepository
     {
+        private readonly SqlConnectionFactory _sqlConnectionFactory;
+
+        public GuestRepository(SqlConnectionFactory sqlConnectionFactory)
+        {
+            _sqlConnectionFactory = sqlConnectionFactory;
+        }
     }
 }
