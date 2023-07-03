6eab8213f73323a619b67338aedf911329f9ff4b
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-03 16:53:19 +0200

    Implement IRoomRepository in RoomRepository

--- HotelManagementSystem.Infrastructure.DataAccess/RoomRepository.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/RoomRepository.cs
@@ -1,12 +1,29 @@
-﻿namespace HotelManagementSystem.Infrastructure.DataAccess
+﻿using Dapper;
+using HotelManagementSystem.Core.DomainServices;
+
+namespace HotelManagementSystem.Infrastructure.DataAccess
 {
-    public class RoomRepository
+    public class RoomRepository : IRoomRepository
     {
         private readonly SqlConnectionFactory _sqlConnectionFactory;
 
         public RoomRepository(SqlConnectionFactory sqlConnectionFactory)
         {
             _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public Guid GetIdByRoomNr(short roomNr)
+        {
+            var sql = @"SELECT id FROM room WHERE room_nr = @RoomNr";
+
+            var parameters = new
+            {
+                RoomNr = roomNr
+            };
+
+            using var conn = _sqlConnectionFactory.CreateSqlConnection();
+            var id = conn.QuerySingleOrDefault<Guid>(sql, parameters);
+            return id;
         }
     }
 }
