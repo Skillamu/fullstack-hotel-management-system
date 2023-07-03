15ddc97723b0f7c2f190cdd2bd45338726ae26fa
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-03 16:31:46 +0200

    Implement IGuestRepository in GuestRepository

--- HotelManagementSystem.Infrastructure.DataAccess/GuestRepository.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/GuestRepository.cs
@@ -1,12 +1,33 @@
-﻿namespace HotelManagementSystem.Infrastructure.DataAccess
+﻿using Dapper;
+using HotelManagementSystem.Core.DomainModel;
+using HotelManagementSystem.Core.DomainServices;
+
+namespace HotelManagementSystem.Infrastructure.DataAccess
 {
-    public class GuestRepository
+    public class GuestRepository : IGuestRepository
     {
         private readonly SqlConnectionFactory _sqlConnectionFactory;
 
         public GuestRepository(SqlConnectionFactory sqlConnectionFactory)
         {
             _sqlConnectionFactory = sqlConnectionFactory;
+        }
+
+        public void Create(Guest guest)
+        {
+            var sql = @"INSERT INTO guest
+                        VALUES (@Id, @FirstName, @LastName, @PhoneNr)";
+
+            var parameters = new
+            {
+                guest.Id,
+                guest.FirstName,
+                guest.LastName,
+                guest.PhoneNr
+            };
+
+            using var conn = _sqlConnectionFactory.CreateSqlConnection();
+            conn.Execute(sql, parameters);
         }
     }
 }
