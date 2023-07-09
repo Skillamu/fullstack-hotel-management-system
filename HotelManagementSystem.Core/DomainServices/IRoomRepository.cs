bfae52529500f71fd91637bc0ae21e0da23e0083
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-09 12:28:54 +0200

    Change return type of method in IRoomRepository to Room

--- HotelManagementSystem.Core/DomainServices/IRoomRepository.cs
+++ HotelManagementSystem.Core/DomainServices/IRoomRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace HotelManagementSystem.Core.DomainServices
+﻿using HotelManagementSystem.Core.DomainModel;
+
+namespace HotelManagementSystem.Core.DomainServices
 {
     public interface IRoomRepository
     {
-        public Guid GetIdByRoomNr(short roomNr);
+        public Room GetRoomByRoomNr(short roomNr);
     }
 }
