a25a57c29244df9a61fa8b3be5e7f0af6fc34593
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-15 12:22:17 +0200

    Add parameterized constructor in Reservation class for object instances

--- HotelManagementSystem.Core/DomainModel/Reservation.cs
+++ HotelManagementSystem.Core/DomainModel/Reservation.cs
@@ -6,6 +6,13 @@
         public Guest Guest { get; private set; }
         public Room Room { get; private set; }
 
+        public Reservation(Guest guest, Room room)
+        {
+            Id = Guid.NewGuid();
+            Guest = guest;
+            Room = room;
+        }
+
         public void AddRoom(Room room)
         {
             Room = room;
