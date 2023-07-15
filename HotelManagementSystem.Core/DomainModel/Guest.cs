d8cffc2677ae95bad23f1b7f41c954d82a6da7fe
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-15 12:30:54 +0200

    Add parameterized constructor in Guest class for object instances

--- HotelManagementSystem.Core/DomainModel/Guest.cs
+++ HotelManagementSystem.Core/DomainModel/Guest.cs
@@ -6,5 +6,13 @@
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public string PhoneNr { get; private set; }
+
+        public Guest(string firstName, string lastName, string phoneNr)
+        {
+            Id = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNr = phoneNr;
+        }
     }
 }
