1b0ad67e80d0e543aa1371592e44de2ddbc95368
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-15 13:01:30 +0200

    Create members in GuestViewModel class

--- HotelManagementSystem.Presentation/ViewModels/GuestViewModel.cs
+++ HotelManagementSystem.Presentation/ViewModels/GuestViewModel.cs
@@ -2,5 +2,15 @@
 {
     public class GuestViewModel
     {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string PhoneNr { get; private set; }
+
+        public GuestViewModel(string firstName, string lastName, string phoneNr)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNr = phoneNr;
+        }
     }
 }
