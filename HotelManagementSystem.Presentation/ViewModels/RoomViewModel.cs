43e6bdb1b24b1e034420a401b38a32dff655713a
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-15 13:03:17 +0200

    Create members in RoomViewModel class

--- HotelManagementSystem.Presentation/ViewModels/RoomViewModel.cs
+++ HotelManagementSystem.Presentation/ViewModels/RoomViewModel.cs
@@ -2,5 +2,11 @@
 {
     public class RoomViewModel
     {
+        public short RoomNr { get; private set; }
+
+        public RoomViewModel(short roomNr)
+        {
+            RoomNr = roomNr;
+        }
     }
 }
