61409de143f42201af180ace185321d94c72687b
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-16 12:56:59 +0200

    Create extension methods for mapping from view model to domain and vice verca in ReservationMapper class (presentation layer)

--- HotelManagementSystem.Presentation/Mappers/ReservationMapper.cs
+++ HotelManagementSystem.Presentation/Mappers/ReservationMapper.cs
@@ -1,6 +1,24 @@
-﻿namespace HotelManagementSystem.Presentation.Mappers
+﻿using HotelManagementSystem.Core.DomainModel;
+using HotelManagementSystem.Presentation.ViewModels;
+
+namespace HotelManagementSystem.Presentation.Mappers
 {
-    public class ReservationMapper
+    public static class ReservationMapper
     {
+        public static Reservation ToDomain(this ReservationViewModel r)
+        {
+            var guest = new Guest(r.Guest.FirstName, r.Guest.LastName, r.Guest.PhoneNr);
+            var room = new Room(r.Room.RoomNr);
+            var reservation = new Reservation(guest, room);
+            return reservation;
+        }
+
+        public static ReservationViewModel ToViewModel(this Reservation r)
+        {
+            var guestViewModel = new GuestViewModel(r.Guest.FirstName, r.Guest.LastName, r.Guest.LastName);
+            var roomViewModel = new RoomViewModel(r.Room.RoomNr);
+            var reservationViewModel = new ReservationViewModel(guestViewModel, roomViewModel);
+            return reservationViewModel;
+        }
     }
 }
