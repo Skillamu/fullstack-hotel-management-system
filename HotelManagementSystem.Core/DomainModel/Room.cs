b82f53420be68620887ea5fb49804af591432eea
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-16 15:05:08 +0200

    Use constructor overloading in Room class to support object instances at different stages in the app

--- HotelManagementSystem.Core/DomainModel/Room.cs
+++ HotelManagementSystem.Core/DomainModel/Room.cs
@@ -5,6 +5,11 @@
         public Guid Id { get; }
         public short RoomNr { get; private set; }
 
+        public Room(Guid id, short roomNr) : this(roomNr)
+        {
+            Id = id;
+        }
+
         public Room(short roomNr)
         {
             RoomNr = roomNr;
