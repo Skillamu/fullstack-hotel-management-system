4eb7af6ec55e19bb2af5a05dd85cb8922619876e
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-18 13:54:25 +0200

    Add members to ReservationEntity

--- HotelManagementSystem.Infrastructure.DataAccess/Entities/ReservationEntity.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/Entities/ReservationEntity.cs
@@ -2,5 +2,15 @@
 {
     public class ReservationEntity
     {
+        public Guid Id { get; }
+        public Guid GuestId { get; }
+        public Guid RoomId { get; }
+
+        public ReservationEntity(Guid id, Guid guestId, Guid roomId)
+        {
+            Id = id;
+            GuestId = guestId;
+            RoomId = roomId;
+        }
     }
 }
