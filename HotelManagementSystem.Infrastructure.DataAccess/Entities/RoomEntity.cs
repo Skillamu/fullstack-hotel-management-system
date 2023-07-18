566594b1a1015e1aeb29c82994874a2d7cd75207
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-18 13:58:42 +0200

    Add members to RoomEntity

--- HotelManagementSystem.Infrastructure.DataAccess/Entities/RoomEntity.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/Entities/RoomEntity.cs
@@ -2,5 +2,13 @@
 {
     public class RoomEntity
     {
+        public Guid Id { get; }
+        public short RoomNr { get; private set; }
+
+        public RoomEntity(Guid id, short roomNr)
+        {
+            Id = id;
+            RoomNr = roomNr;
+        }
     }
 }
