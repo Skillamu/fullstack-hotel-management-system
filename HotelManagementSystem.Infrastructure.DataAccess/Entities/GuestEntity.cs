c6ccf4f07643404e5f2184eab093f8df81f524a9
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-18 13:56:45 +0200

    Add members to GuestEntity

--- HotelManagementSystem.Infrastructure.DataAccess/Entities/GuestEntity.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/Entities/GuestEntity.cs
@@ -2,5 +2,17 @@
 {
     public class GuestEntity
     {
+        public Guid Id { get; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string PhoneNr { get; private set; }
+
+        public GuestEntity(Guid id, string firstname, string lastName, string phoneNr)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastName;
+            PhoneNr = phoneNr;
+        }
     }
 }
