3a40d0f3dfd9db3103500f16498e94bc34cd514e
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-18 14:37:23 +0200

    Refactor code to return RoomEntity in query and use extension method for mapping in RoomRepository

--- HotelManagementSystem.Infrastructure.DataAccess/Repositories/RoomRepository.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/Repositories/RoomRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Dapper;
 using HotelManagementSystem.Core.DomainModel;
 using HotelManagementSystem.Core.DomainServices;
+using HotelManagementSystem.Infrastructure.DataAccess.Entities;
 using HotelManagementSystem.Infrastructure.DataAccess.Factories;
+using HotelManagementSystem.Infrastructure.DataAccess.Mappers;
 
 namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
 {
@@ -18,13 +20,11 @@ namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
         {
             var sql = @"SELECT id AS Id, room_nr AS RoomNr FROM room WHERE room_nr = @RoomNr";
 
-            var parameters = new
-            {
-                RoomNr = roomNr
-            };
+            var parameters = new { RoomNr = roomNr };
 
             using var conn = _sqlConnectionFactory.CreateSqlConnection();
-            var room = conn.QuerySingleOrDefault<Room>(sql, parameters);
+            var roomEntity = conn.QuerySingleOrDefault<RoomEntity>(sql, parameters);
+            var room = roomEntity.ToDomain();
             return room;
         }
     }
