950001c6090f3997fcad6d93c76850114defabd8
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-18 14:09:26 +0200

    Create extension method in RoomMapper for entity-to-domain mapping

--- HotelManagementSystem.Infrastructure.DataAccess/Mappers/RoomMapper.cs
+++ HotelManagementSystem.Infrastructure.DataAccess/Mappers/RoomMapper.cs
@@ -1,6 +1,19 @@
-﻿namespace HotelManagementSystem.Infrastructure.DataAccess.Mappers
+﻿using HotelManagementSystem.Core.DomainModel;
+using HotelManagementSystem.Infrastructure.DataAccess.Entities;
+
+namespace HotelManagementSystem.Infrastructure.DataAccess.Mappers
 {
-    public class RoomMapper
+    public static class RoomMapper
     {
+        public static Room ToDomain(this RoomEntity r)
+        {
+            if (r == null)
+            {
+                return null;
+            }
+
+            var room = new Room(r.Id, r.RoomNr);
+            return room;
+        }
     }
 }
