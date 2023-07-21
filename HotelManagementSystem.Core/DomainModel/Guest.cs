d6801af10f359d9e1ee28807f3d9c40701f0c5bb
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-21 16:03:10 +0200

    Create public static factory method in Guest class with logic that ensures valid object instance

--- HotelManagementSystem.Core/DomainModel/Guest.cs
+++ HotelManagementSystem.Core/DomainModel/Guest.cs
@@ -7,12 +7,22 @@
         public string LastName { get; private set; }
         public string PhoneNr { get; private set; }
 
-        public Guest(string firstName, string lastName, string phoneNr)
+        private Guest(Guid id, string firstName, string lastName, string phoneNr)
         {
-            Id = Guid.NewGuid();
+            Id = id;
             FirstName = firstName;
             LastName = lastName;
             PhoneNr = phoneNr;
+        }
+
+        public static Guest Create(Guid id, string firstName, string lastName, string phoneNr)
+        {
+            if (id == Guid.Empty || string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || string.IsNullOrEmpty(phoneNr))
+            {
+                return null;
+            }
+
+            return new Guest(id, firstName, lastName, phoneNr);
         }
     }
 }
