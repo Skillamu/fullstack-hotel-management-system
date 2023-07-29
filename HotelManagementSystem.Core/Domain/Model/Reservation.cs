74631ec90df0fbae519986a7c5fda94ab1b833e6
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-29 20:38:04 +0200

    Add property for the DateRange value object in the Reservation entity

--- HotelManagementSystem.Core/Domain/Model/Reservation.cs
+++ HotelManagementSystem.Core/Domain/Model/Reservation.cs
@@ -1,26 +1,30 @@
-﻿namespace HotelManagementSystem.Core.Domain.Model
+﻿using HotelManagementSystem.Core.Domain.ValueObjects;
+
+namespace HotelManagementSystem.Core.Domain.Model
 {
     public class Reservation
     {
         public Guid Id { get; }
         public Guest Guest { get; private set; }
         public Room Room { get; private set; }
+        public DateRange DateRange { get; private set; }
 
-        private Reservation(Guid id, Guest guest, Room room)
+        private Reservation(Guid id, Guest guest, Room room, DateRange dateRange)
         {
             Id = id;
             Guest = guest;
             Room = room;
+            DateRange = dateRange;
         }
 
-        public static Reservation Create(Guid id, Guest guest, Room room)
+        public static Reservation Create(Guid id, Guest guest, Room room, DateRange dateRange)
         {
-            if (id == Guid.Empty || guest == null || room == null)
+            if (id == Guid.Empty || guest == null || room == null || dateRange == null)
             {
                 return null;
             }
 
-            return new Reservation(id, guest, room);
+            return new Reservation(id, guest, room, dateRange);
         }
     }
 }
