efaa49f80032a5ab6f9982c0fde82645e320f3ae
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-30 13:08:07 +0200

    Create method to check room availability within a date range in IRoomRepository

--- HotelManagementSystem.Core/Domain/Services/IRoomRepository.cs
+++ HotelManagementSystem.Core/Domain/Services/IRoomRepository.cs
@@ -1,9 +1,11 @@
 ﻿using HotelManagementSystem.Core.Domain.Model;
+using HotelManagementSystem.Core.Domain.ValueObjects;
 
 namespace HotelManagementSystem.Core.Domain.Services
 {
     public interface IRoomRepository
     {
-        public Room GetRoomByRoomNr(short roomNr);
+        Room GetRoomByRoomNr(short roomNr);
+        bool IsRoomAvailableWithinDateRange(string roomNr, DateRange dateRange);
     }
 }
