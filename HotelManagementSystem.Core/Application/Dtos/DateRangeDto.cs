c33d4d8c1e8122d9101f87605bb5788ddad37bf3
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-07-30 13:16:39 +0200

    Create DateRangeDto class with properties for check-in and check-out dates

---
+++ HotelManagementSystem.Core/Application/Dtos/DateRangeDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelManagementSystem.Core.Application.Dtos
+{
+    public class DateRangeDto
+    {
+        public string CheckInDate { get; private set; }
+        public string CheckOutDate { get; private set;}
+
+        public DateRangeDto(string checkInDate, string checkOutDate)
+        {
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+        }
+    }
+}
