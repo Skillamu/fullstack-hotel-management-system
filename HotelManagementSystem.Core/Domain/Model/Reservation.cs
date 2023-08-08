96d28cf095d0065f0d9de2b997541fec0fa278fc
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-08 11:40:23 +0200

    Add properties CreatedAtDate and IsVerified to Reservation class and implement business logic to ensure valid dates

--- HotelManagementSystem.Core/Domain/Model/Reservation.cs
+++ HotelManagementSystem.Core/Domain/Model/Reservation.cs
@@ -8,23 +8,41 @@ namespace HotelManagementSystem.Core.Domain.Model
         public Guest Guest { get; private set; }
         public Room Room { get; private set; }
         public DateRange DateRange { get; private set; }
+        public CreatedAtDate CreatedAtDate { get; private set; }
+        public bool IsVerified { get; private set; }
 
-        private Reservation(Guid id, Guest guest, Room room, DateRange dateRange)
+        private Reservation(Guid id, Guest guest, Room room, DateRange dateRange, CreatedAtDate createdAtDate, bool isVerified)
         {
             Id = id;
             Guest = guest;
             Room = room;
             DateRange = dateRange;
+            CreatedAtDate = createdAtDate;
+            IsVerified = isVerified;
         }
 
-        public static Reservation Create(Guid id, Guest guest, Room room, DateRange dateRange)
+        public static Reservation Create(Guest guest, Room room, DateRange dateRange)
         {
-            if (id == Guid.Empty || guest == null || room == null || dateRange == null)
+            var id = Guid.NewGuid();
+            var createdAtDate = CreatedAtDate.Create(DateTime.Now);
+            var isVerified = false;
+
+            if (id == Guid.Empty || guest == null || room == null || dateRange == null || createdAtDate == null || createdAtDate.Date > dateRange.CheckInDate)
             {
                 return null;
             }
 
-            return new Reservation(id, guest, room, dateRange);
+            return new Reservation(id, guest, room, dateRange, createdAtDate, isVerified);
+        }
+
+        public static Reservation Create(Guid id, Guest guest, Room room, DateRange dateRange, CreatedAtDate createdAtDate, bool isVerified)
+        {
+            if (id == Guid.Empty || guest == null || room == null || dateRange == null || createdAtDate == null || createdAtDate.Date > dateRange.CheckInDate)
+            {
+                return null;
+            }
+
+            return new Reservation(id, guest, room, dateRange, createdAtDate, isVerified);
         }
     }
 }
