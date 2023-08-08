c67d49843c094fc4eb65263efcf62a6cdd92f0e3
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-08 10:50:38 +0200

    Implement members and business logic in the CreatedAtDate value object

--- HotelManagementSystem.Core/Domain/ValueObjects/CreatedAtDate.cs
+++ HotelManagementSystem.Core/Domain/ValueObjects/CreatedAtDate.cs
@@ -2,5 +2,48 @@
 {
     public class CreatedAtDate
     {
+        public DateTime Date { get; }
+
+        private CreatedAtDate(DateTime date)
+        {
+            Date = date;
+        }
+
+        public static CreatedAtDate Create()
+        {
+            var date = DateTime.Now;
+
+            return IsValidDate(date) ? new CreatedAtDate(date) : null;
+        }
+
+        public static CreatedAtDate Create(DateTime date)
+        {
+            return IsValidDate(date) ? new CreatedAtDate(date) : null;
+        }
+
+        private static bool IsValidDate(DateTime date)
+        {
+            if (!DateTime.IsLeapYear(date.Year) && date.Month == 2 && date.Day == 29)
+            {
+                return false;
+            }
+
+            if (date.Year < DateTime.Now.Year)
+            {
+                return false;
+            }
+
+            if (date.Year == DateTime.Now.Year && date.Month < DateTime.Now.Month)
+            {
+                return false;
+            }
+
+            if (date.Year == DateTime.Now.Year && date.Month == DateTime.Now.Month && date.Day < DateTime.Now.Day)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
