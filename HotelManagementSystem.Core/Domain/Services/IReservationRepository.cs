4d3d2a362b7d20e0a6f5d92239ebd042d788bdf8
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-08 13:47:36 +0200

    Create additional CRUD methods in IReservationRepository

--- HotelManagementSystem.Core/Domain/Services/IReservationRepository.cs
+++ HotelManagementSystem.Core/Domain/Services/IReservationRepository.cs
@@ -5,5 +5,9 @@ namespace HotelManagementSystem.Core.Domain.Services
     public interface IReservationRepository
     {
         void Create(Reservation reservation);
+        void Update(Reservation reservation);
+        void Delete(Reservation reservation);
+        Reservation GetUnverifiedReservationByGuestPhoneNr(string phoneNr);
+        IEnumerable<Reservation> GetUnverifiedReservations();
     }
 }
