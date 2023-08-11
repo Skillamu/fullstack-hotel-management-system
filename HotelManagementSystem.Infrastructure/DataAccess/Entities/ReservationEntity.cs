fd6635a9de73827a7f9f4a616f941e8241cedfec
Skillamu <ps4.c.aa@gmail.com>
2023-08-11 12:15:50 +0200

    Align entities with the tables in the database

--- HotelManagementSystem.Infrastructure/DataAccess/Entities/ReservationEntity.cs
+++ HotelManagementSystem.Infrastructure/DataAccess/Entities/ReservationEntity.cs
@@ -2,10 +2,23 @@
 {
     public class ReservationEntity
     {
-        public Guid Id { get; set; }
-        public Guid GuestId { get; set; }
-        public Guid RoomId { get; set; }
-        public DateTime CheckInDate { get; set; }
-        public DateTime CheckOutDate { get; set; }
+        public Guid Id { get; }
+        public Guid GuestId { get; }
+        public Guid RoomId { get; }
+        public DateTime CheckInDate { get; }
+        public DateTime CheckOutDate { get; }
+        public DateTime CreateDate { get; }
+        public bool IsVerified { get; }
+
+        public ReservationEntity(Guid id, Guid guestId, Guid roomId, DateTime checkInDate, DateTime checkOutDate, DateTime createDate, bool isVerified)
+        {
+            Id = id;
+            GuestId = guestId;
+            RoomId = roomId;
+            CheckInDate = checkInDate;
+            CheckOutDate = checkOutDate;
+            CreateDate = createDate;
+            IsVerified = isVerified;
+        }
     }
 }
