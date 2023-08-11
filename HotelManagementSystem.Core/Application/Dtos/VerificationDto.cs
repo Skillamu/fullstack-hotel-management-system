a95c596ce586628c9e58950a918b0431984348a0
Skillamu <ps4.c.aa@gmail.com>
2023-08-11 11:46:05 +0200

    Create VerificationDto class that will be used for verification requests

---
+++ HotelManagementSystem.Core/Application/Dtos/VerificationDto.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelManagementSystem.Core.Application.Dtos
+{
+    public class VerificationDto
+    {
+        public string PhoneNr { get; private set; }
+        public string VerificationCode { get; private set; }
+
+        public VerificationDto(string phoneNr, string verificationCode)
+        {
+            PhoneNr = phoneNr;
+            VerificationCode = verificationCode;
+        }
+    }
+}
