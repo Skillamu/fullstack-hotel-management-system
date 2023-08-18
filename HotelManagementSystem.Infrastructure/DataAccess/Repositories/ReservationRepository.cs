fa7b119a32fd430a0c443104ffb952854874c285
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-18 13:14:58 +0200

    Implement method for getting an unverified reservation by phone number in ReservationRepository class

--- HotelManagementSystem.Infrastructure/DataAccess/Repositories/ReservationRepository.cs
+++ HotelManagementSystem.Infrastructure/DataAccess/Repositories/ReservationRepository.cs
@@ -3,6 +3,7 @@ using HotelManagementSystem.Core.Domain.Services;
 using HotelManagementSystem.Core.Domain.Model;
 using HotelManagementSystem.Infrastructure.DataAccess.Factories;
 using HotelManagementSystem.Infrastructure.DataAccess.Mappers;
+using HotelManagementSystem.Infrastructure.DataAccess.Entities;
 
 namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
 {
@@ -32,6 +33,29 @@ namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
 
             using var conn = _sqlConnectionFactory.CreateSqlConnection();
             conn.Execute(sql, parameters);
+        }
+
+        public Reservation? GetUnverifiedReservationByPhoneNr(string phoneNr)
+        {
+            var sql = @"SELECT reservation.id AS Id, check_in_date AS CheckInDate, check_out_date AS CheckOutDate,
+                        created_at_date AS CreatedAtdate, is_verified AS IsVerified,
+                        guest_id AS Id, first_name AS FirstName, last_name AS LastName, phone_nr AS PhoneNr,
+                        room_id AS Id, room_nr AS RoomNr
+                        FROM reservation
+                        JOIN guest ON reservation.guest_id = guest.id
+                        JOIN room ON reservation.room_id = room.id
+                        WHERE phone_nr = @PhoneNr AND is_verified = 'False'";
+
+            using var conn = _sqlConnectionFactory.CreateSqlConnection();
+
+            var reservation = conn.Query<ReservationEntity, GuestEntity, RoomEntity, Reservation>(sql, (reservationEntity, guestEntity, roomEntity) =>
+            {
+                return ReservationMapper.ToDomain(reservationEntity, guestEntity, roomEntity);
+            },
+            param: new { PhoneNr = phoneNr },
+            splitOn: "Id, Id").SingleOrDefault();
+
+            return reservation;
         }
     }
 }
