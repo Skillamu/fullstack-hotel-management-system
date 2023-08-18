e5f382cf99ac0d379fab53eab60e5f7735d5a14a
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-18 16:43:51 +0200

    Refactor DateRange class to enhance readability and avoid unnecessary complexity

--- HotelManagementSystem.Core/Domain/ValueObjects/DateRange.cs
+++ HotelManagementSystem.Core/Domain/ValueObjects/DateRange.cs
@@ -13,52 +13,12 @@
 
         public static DateRange Create(DateTime checkInDate, DateTime checkOutDate)
         {
-            if (!IsValidDate(checkInDate) || !IsValidDate(checkOutDate))
-            {
-                return null;
-            }
-
-            if (!IsValidDateRange(checkInDate, checkOutDate))
+            if (checkInDate < DateTime.Now || checkOutDate < checkInDate)
             {
                 return null;
             }
 
             return new DateRange(checkInDate, checkOutDate);
-        }
-
-        public static bool IsValidDate(DateTime date)
-        {
-            if (!DateTime.IsLeapYear(date.Year) && date.Month == 2 && date.Day == 29)
-            {
-                return false;
-            }
-
-            if (date.Year < DateTime.Now.Year)
-            {
-                return false;
-            }
-
-            if (date.Year == DateTime.Now.Year && date.Month < DateTime.Now.Month)
-            {
-                return false;
-            }
-
-            if (date.Year == DateTime.Now.Year && date.Month == DateTime.Now.Month && date.Day < DateTime.Now.Day)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public static bool IsValidDateRange(DateTime checkInDate, DateTime checkOutDate)
-        {
-            if (checkOutDate < checkInDate)
-            {
-                return false;
-            }
-
-            return true;
         }
     }
 }
