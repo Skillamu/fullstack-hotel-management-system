e36f4b6ef218dc4169e002483537e970bdda07be
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-19 12:35:44 +0200

    Implement method for getting a guest by phone nr in GuestRepository class

--- HotelManagementSystem.Infrastructure/DataAccess/Repositories/GuestRepository.cs
+++ HotelManagementSystem.Infrastructure/DataAccess/Repositories/GuestRepository.cs
@@ -2,7 +2,6 @@
 using HotelManagementSystem.Core.Domain.Services;
 using HotelManagementSystem.Core.Domain.Model;
 using HotelManagementSystem.Infrastructure.DataAccess.Factories;
-using HotelManagementSystem.Infrastructure.DataAccess.Mappers;
 
 namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
 {
@@ -20,10 +19,29 @@ namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
             var sql = @"INSERT INTO guest
                         VALUES (@Id, @FirstName, @LastName, @PhoneNr)";
 
-            var guestEntity = guest.ToEntity();
+            var parameters = new
+            {
+                Id = guest.Id,
+                FirstName = guest.FirstName,
+                LastName = guest.LastName,
+                PhoneNr = guest.PhoneNr
+            };
 
             using var conn = _sqlConnectionFactory.CreateSqlConnection();
-            conn.Execute(sql, guestEntity);
+            conn.Execute(sql, parameters);
+        }
+
+        public Guest GetGuestByPhoneNr(string phoneNr)
+        {
+            var sql = @"SELECT id AS Id, first_name AS FirstName, last_name AS LastName, phone_nr AS PhoneNr
+                        FROM guest WHERE phone_nr = @PhoneNr";
+
+            var parameters = new { PhoneNr = phoneNr };
+
+            using var conn = _sqlConnectionFactory.CreateSqlConnection();
+            var guest = conn.QuerySingleOrDefault<Guest>(sql, parameters);
+
+            return guest;
         }
     }
 }
