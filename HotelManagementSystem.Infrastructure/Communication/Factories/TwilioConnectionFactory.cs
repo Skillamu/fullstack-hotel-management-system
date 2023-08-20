0bd10dc9beb8a479f978baf5b2a5d89c83115d58
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-20 17:40:51 +0200

    Create methods for creating necessary verification resources in TwilioConnectionFactory class

--- HotelManagementSystem.Infrastructure/Communication/Factories/TwilioConnectionFactory.cs
+++ HotelManagementSystem.Infrastructure/Communication/Factories/TwilioConnectionFactory.cs
@@ -1,6 +1,39 @@
-﻿namespace HotelManagementSystem.Infrastructure.Communication.Factories
+﻿using Twilio;
+using Twilio.Rest.Verify.V2.Service;
+
+namespace HotelManagementSystem.Infrastructure.Communication.Factories
 {
     public class TwilioConnectionFactory
     {
+        private readonly string _verificationServiceSid;
+
+        public TwilioConnectionFactory(string accountSid, string authToken, string verificationServiceSid)
+        {
+            TwilioClient.Init(accountSid, authToken);
+
+            _verificationServiceSid = verificationServiceSid;
+        }
+
+        public VerificationResource CreateVerificationResource(string phoneNr)
+        {
+            var verification = VerificationResource.Create(
+                to: $"+47{phoneNr}",
+                channel: "sms",
+                pathServiceSid: _verificationServiceSid
+                );
+
+            return verification;
+        }
+
+        public VerificationCheckResource CreateVerificationCheckResource(string phoneNr, string verificationCode)
+        {
+            var verificationCheck = VerificationCheckResource.Create(
+                to: $"+47{phoneNr}",
+                code: $"{verificationCode}",
+                pathServiceSid: _verificationServiceSid
+                );
+
+            return verificationCheck;
+        }
     }
 }
