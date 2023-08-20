6be26f6fc874c249952d79566c3d9b87f999a7ed
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-20 18:10:52 +0200

    Implement the domain service IVerificationService in the VerificationService class

--- HotelManagementSystem.Infrastructure/Communication/Services/VerificationService.cs
+++ HotelManagementSystem.Infrastructure/Communication/Services/VerificationService.cs
@@ -1,8 +1,27 @@
 ﻿using HotelManagementSystem.Core.Domain.Services;
+using HotelManagementSystem.Infrastructure.Communication.Factories;
 
 namespace HotelManagementSystem.Infrastructure.Communication.Verification
 {
     public class VerificationService : IVerificationService
     {
+        private readonly TwilioClientFactory _twilioClientFactory;
+
+        public VerificationService(TwilioClientFactory twilioClientFactory)
+        {
+            _twilioClientFactory = twilioClientFactory;
+        }
+
+        public void Send(string phoneNr)
+        {
+            _twilioClientFactory.CreateVerificationResource(phoneNr);
+        }
+
+        public bool Verify(string phoneNr, string verificationCode)
+        {
+            var verificationCheck = _twilioClientFactory.CreateVerificationCheckResource(phoneNr, verificationCode);
+
+            return verificationCheck.Status == "approved";
+        }
     }
 }
