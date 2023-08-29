8a7d7c1f53ebc590fe39d98c1cbc617f2d81d3d7
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-29 12:58:23 +0200

    Remove old code and implement method for getting a room with the chosen attributes and within the chosen date range

--- HotelManagementSystem.Infrastructure/DataAccess/Repositories/RoomRepository.cs
+++ HotelManagementSystem.Infrastructure/DataAccess/Repositories/RoomRepository.cs
@@ -1,10 +1,8 @@
 ﻿using Dapper;
 using HotelManagementSystem.Core.Domain.Services;
-using HotelManagementSystem.Core.Domain.Model;
-using HotelManagementSystem.Infrastructure.DataAccess.Entities;
 using HotelManagementSystem.Infrastructure.DataAccess.Factories;
-using HotelManagementSystem.Infrastructure.DataAccess.Mappers;
 using HotelManagementSystem.Core.Domain.ValueObjects;
+using HotelManagementSystem.Core.Domain.Model;
 
 namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
 {
@@ -17,26 +15,26 @@ namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
             _sqlConnectionFactory = sqlConnectionFactory;
         }
 
-        public Room GetAvailableRoomWithinDateRange(short roomNr, DateRange dateRange)
+        public Room GetRoomWithinDateRange(bool hasCityView, DateRange dateRange)
         {
-            var sql = @"SELECT room.id AS _id, room.room_nr AS RoomNr 
+            var sql = @"SELECT room.id AS _id, room.room_nr AS RoomNr, has_city_view AS HasCityView
                         FROM room
                         LEFT JOIN reservation
                         ON room.id = reservation.room_id
                         AND reservation.check_in_date <= @CheckOutDate
                         AND reservation.check_out_date >= @CheckInDate
                         WHERE reservation.room_id IS NULL
-                        AND room.room_nr = @RoomNr;";
+                        AND room.has_city_view = @HasCityView";
 
             var parameters = new
             {
-                RoomNr = roomNr,
+                HasCityView = hasCityView,
                 CheckInDate = dateRange.CheckInDate,
                 CheckOutDate = dateRange.CheckOutDate
             };
 
             using var conn = _sqlConnectionFactory.CreateSqlConnection();
-            var room = conn.QuerySingleOrDefault<Room>(sql, parameters);
+            var room = conn.QueryFirstOrDefault<Room>(sql, parameters);
 
             return room;
         }
