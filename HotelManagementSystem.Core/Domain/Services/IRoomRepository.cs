d4a25c0f83bab02f27fc0df17726e0b82bb2c42f
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-29 12:05:00 +0200

    Add method for getting a room by its attributes within date range

--- HotelManagementSystem.Core/Domain/Services/IRoomRepository.cs
+++ HotelManagementSystem.Core/Domain/Services/IRoomRepository.cs
@@ -5,6 +5,6 @@ namespace HotelManagementSystem.Core.Domain.Services
 {
     public interface IRoomRepository
     {
-        Room GetAvailableRoomWithinDateRange(short roomNr, DateRange dateRange);
+        Room GetRoomWithinDateRange(bool hasCityView, DateRange dateRange);
     }
 }
