61ad6af188dfd8ab6fad55d7a7791a9c26ba17f1
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-08-31 18:17:00 +0200

    Delete method to check if a guest already exists in IGuestRepository, it's not necessary anymore

--- HotelManagementSystem.Core/Domain/Services/IGuestRepository.cs
+++ HotelManagementSystem.Core/Domain/Services/IGuestRepository.cs
@@ -7,6 +7,5 @@ namespace HotelManagementSystem.Core.Domain.Services
         void Create(Guest guest);
         void Delete(Guest guest);
         Guest GetGuestByPhoneNr(string phoneNr);
-        bool GuestByPhoneNrAlreadyExists(string phoneNr);
     }
 }
