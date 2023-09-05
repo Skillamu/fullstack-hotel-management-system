409d920d9993b32b451be64c468f98b4e37039cf
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-05 15:51:43 +0200

    Create DTO for RoomType

---
+++ HotelManagementSystem.Core/Application/Dtos/RoomTypeDto.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelManagementSystem.Core.Application.Dtos
+{
+    public class RoomTypeDto
+    {
+        public string Type { get; private set; }
+        public bool HasCityView { get; private set; }
+        public bool HasBathtub { get; private set; }
+
+        public RoomTypeDto(string type, bool hasCityView, bool hasBathtub)
+        {
+            Type = type;
+            HasCityView = hasCityView;
+            HasBathtub = hasBathtub;
+        }
+    }
+}
