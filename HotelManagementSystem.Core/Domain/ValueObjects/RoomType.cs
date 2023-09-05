d05b3208bb77d28ccac414c25fe076f2e83435e3
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-05 16:16:01 +0200

    Refactor to enhance clarity of the code

--- HotelManagementSystem.Core/Domain/ValueObjects/RoomType.cs
+++ HotelManagementSystem.Core/Domain/ValueObjects/RoomType.cs
@@ -3,16 +3,18 @@
     public class RoomType
     {
         public string Type { get; }
-        public bool HasCityView => Type == "Superior" || Type == "Deluxe";
-        public bool HasBathtub => Type == "Deluxe";
+        public bool HasCityView { get; }
+        public bool HasBathtub { get; }
 
-        private RoomType(string type)
+        private RoomType(string type, bool hasCityView, bool hasBathtub)
         {
             Type = type;
+            HasCityView = hasCityView;
+            HasBathtub = hasBathtub;
         }
 
-        public static RoomType Standard => new RoomType("Standard");
-        public static RoomType Superior => new RoomType("Superior");
-        public static RoomType Deluxe => new RoomType("Deluxe");
+        public static RoomType Standard => new RoomType("Standard", false, false);
+        public static RoomType Superior => new RoomType("Superior", true, false);
+        public static RoomType Deluxe => new RoomType("Deluxe", true, true);
     }
 }
