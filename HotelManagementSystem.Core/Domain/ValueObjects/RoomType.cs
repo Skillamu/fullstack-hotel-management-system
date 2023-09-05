ff90f40f7716a46968fd72c43c1a894d66fb39ce
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-05 10:28:32 +0200

    Implement members and business logic which decides what attributes each room type has

--- HotelManagementSystem.Core/Domain/ValueObjects/RoomType.cs
+++ HotelManagementSystem.Core/Domain/ValueObjects/RoomType.cs
@@ -2,5 +2,17 @@
 {
     public class RoomType
     {
+        public string Type { get; }
+        public bool HasCityView => Type == "Superior" || Type == "Deluxe";
+        public bool HasBathtub => Type == "Deluxe";
+
+        private RoomType(string type)
+        {
+            Type = type;
+        }
+
+        public static RoomType Standard => new RoomType("Standard");
+        public static RoomType Superior => new RoomType("Superior");
+        public static RoomType Deluxe => new RoomType("Deluxe");
     }
 }
