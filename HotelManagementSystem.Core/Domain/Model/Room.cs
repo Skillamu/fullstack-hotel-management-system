e4621b6c5c87b1f5d74c9cd4a9714a5a089cc6e6
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-05 10:49:14 +0200

    Add property RoomType that will contain the attributes of the room

--- HotelManagementSystem.Core/Domain/Model/Room.cs
+++ HotelManagementSystem.Core/Domain/Model/Room.cs
@@ -1,11 +1,13 @@
-﻿namespace HotelManagementSystem.Core.Domain.Model
+﻿using HotelManagementSystem.Core.Domain.ValueObjects;
+
+namespace HotelManagementSystem.Core.Domain.Model
 {
     public class Room
     {
         private readonly Guid _id;
         public Guid Id => _id;
         public short RoomNr { get; private set; }
-        public bool HasCityView { get; private set; }
+        public RoomType RoomType { get; private set; }
 
         private Room()
         {
