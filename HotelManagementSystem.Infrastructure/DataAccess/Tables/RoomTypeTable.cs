c41188eaad6429cf39eb4a3ee776b924cc92d818
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-06 15:28:01 +0200

    Create a table class representing the room_type table in the database

---
+++ HotelManagementSystem.Infrastructure/DataAccess/Tables/RoomTypeTable.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagementSystem.Infrastructure.DataAccess.Entities
+{
+    public class RoomTypeTable
+    {
+        public Guid Id { get; }
+        public string Type { get; }
+        public bool HasCityView { get; }
+        public bool HasBathtub { get; }
+    }
+}
