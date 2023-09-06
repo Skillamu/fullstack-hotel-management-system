b7e99f528a4080c607fc431639bb909c2e62b06e
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-06 17:42:08 +0200

    Refactor and update reservation mapping from tables to the domain

--- HotelManagementSystem.Infrastructure/DataAccess/Mappers/ReservationMapper.cs
+++ HotelManagementSystem.Infrastructure/DataAccess/Mappers/ReservationMapper.cs
@@ -1,6 +1,6 @@
 ﻿using HotelManagementSystem.Core.Domain.Model;
 using HotelManagementSystem.Core.Domain.ValueObjects;
-using HotelManagementSystem.Infrastructure.DataAccess.Entities;
+using HotelManagementSystem.Infrastructure.DataAccess.Tables;
 using System.Reflection;
 
 namespace HotelManagementSystem.Infrastructure.DataAccess.Mappers
@@ -10,73 +10,55 @@ namespace HotelManagementSystem.Infrastructure.DataAccess.Mappers
         // NOTE!
         // Reflection is used here due to the lack of Dapper functionality when it comes to mapping multiple
         // tables to one object that is well encapsulated with private setters
-        public static Reservation ToDomain(ReservationEntity reservationEntity, GuestEntity guestEntity, RoomEntity roomEntity)
+        public static Reservation ToDomain(ReservationTable reservationTable, GuestTable guestTable, RoomTable roomTable, RoomTypeTable roomTypeTable)
         {
             var reservationType = typeof(Reservation);
             var ctor = reservationType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, new Type[0]);
             var reservation = (Reservation)ctor.Invoke(null);
 
             var reservationTypeIdBackField = reservationType.GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
-            reservationTypeIdBackField.SetValue(reservation, reservationEntity.Id);
+            reservationTypeIdBackField.SetValue(reservation, reservationTable.Id);
 
             var reservationTypeGuestProperty = reservationType.GetProperty("Guest", BindingFlags.Instance | BindingFlags.Public);
-            reservationTypeGuestProperty.SetValue(reservation, IntanceOfGuestWithReflection(guestEntity));
+            reservationTypeGuestProperty.SetValue(reservation, ToDomain(guestTable));
 
             var reservationTypeRoomProperty = reservationType.GetProperty("Room", BindingFlags.Instance | BindingFlags.Public);
-            reservationTypeRoomProperty.SetValue(reservation, InstanceOfRoomWithReflection(roomEntity));
+            reservationTypeRoomProperty.SetValue(reservation, RoomMapper.ToDomain(roomTable, roomTypeTable));
 
             var reservationTypeDateRangeProperty = reservationType.GetProperty("DateRange", BindingFlags.Instance | BindingFlags.Public);
-            reservationTypeDateRangeProperty.SetValue(reservation, InstanceOfDateRangeWithReflection(reservationEntity.CheckInDate, reservationEntity.CheckOutDate));
+            reservationTypeDateRangeProperty.SetValue(reservation, ToDomain(reservationTable.CheckInDate, reservationTable.CheckOutDate));
 
             var reservationTypeCreatedAtDateProperty = reservationType.GetProperty("CreatedAtDate", BindingFlags.Instance | BindingFlags.Public);
-            reservationTypeCreatedAtDateProperty.SetValue(reservation, InstanceOfCreatedAtDateWithReflection(reservationEntity.CreatedAtDate));
+            reservationTypeCreatedAtDateProperty.SetValue(reservation, ToDomain(reservationTable.CreatedAtDate));
 
             var reservationTypeIsVerifiedProperty = reservationType.GetProperty("IsVerified", BindingFlags.Instance | BindingFlags.Public);
-            reservationTypeIsVerifiedProperty.SetValue(reservation, reservationEntity.IsVerified);
+            reservationTypeIsVerifiedProperty.SetValue(reservation, reservationTable.IsVerified);
 
             return reservation;
         }
 
-        private static Guest IntanceOfGuestWithReflection(GuestEntity guestEntity)
+        private static Guest ToDomain(GuestTable guestTable)
         {
             var guestType = typeof(Guest);
             var ctor = guestType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, new Type[0]);
             var guest = (Guest)ctor.Invoke(null);
 
             var guestTypeIdBackField = guestType.GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
-            guestTypeIdBackField.SetValue(guest, guestEntity.Id);
+            guestTypeIdBackField.SetValue(guest, guestTable.Id);
 
             var guestTypeFirstNameProperty = guestType.GetProperty("FirstName", BindingFlags.Instance | BindingFlags.Public);
-            guestTypeFirstNameProperty.SetValue(guest, guestEntity.FirstName);
+            guestTypeFirstNameProperty.SetValue(guest, guestTable.FirstName);
 
             var guestTypeLastNameProperty = guestType.GetProperty("LastName", BindingFlags.Instance | BindingFlags.Public);
-            guestTypeLastNameProperty.SetValue(guest, guestEntity.LastName);
+            guestTypeLastNameProperty.SetValue(guest, guestTable.LastName);
 
             var guestTypePhoneNrProperty = guestType.GetProperty("PhoneNr", BindingFlags.Instance | BindingFlags.Public);
-            guestTypePhoneNrProperty.SetValue(guest, guestEntity.PhoneNr);
+            guestTypePhoneNrProperty.SetValue(guest, guestTable.PhoneNr);
 
             return guest;
         }
 
-        private static Room InstanceOfRoomWithReflection(RoomEntity roomEntity)
-        {
-            var roomType = typeof(Room);
-            var ctor = roomType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, new Type[0]);
-            var room = (Room)ctor.Invoke(null);
-
-            var roomIdBackField = roomType.GetField("_id", BindingFlags.Instance | BindingFlags.NonPublic);
-            roomIdBackField.SetValue(room, roomEntity.Id);
-
-            var roomRoomNrProperty = roomType.GetProperty("RoomNr", BindingFlags.Instance | BindingFlags.Public);
-            roomRoomNrProperty.SetValue(room, roomEntity.RoomNr);
-
-            var roomHasCityViewProperty = roomType.GetProperty("HasCityView", BindingFlags.Instance | BindingFlags.Public);
-            roomHasCityViewProperty.SetValue(room, roomEntity.HasCityView);
-
-            return room;
-        }
-
-        private static DateRange InstanceOfDateRangeWithReflection(DateTime checkInDate, DateTime checkOutDate)
+        private static DateRange ToDomain(DateTime checkInDate, DateTime checkOutDate)
         {
             var dateRangeType = typeof(DateRange);
             var ctor = dateRangeType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, new Type[] { typeof(DateTime), typeof(DateTime) });
@@ -85,7 +67,7 @@ namespace HotelManagementSystem.Infrastructure.DataAccess.Mappers
             return dateRange;
         }
 
-        private static CreatedAtDate InstanceOfCreatedAtDateWithReflection(DateTime value)
+        private static CreatedAtDate ToDomain(DateTime value)
         {
             var createdAtDateType = typeof(CreatedAtDate);
             var ctor = createdAtDateType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic, new Type[] { typeof(DateTime) });
