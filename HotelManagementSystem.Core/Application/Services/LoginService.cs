cf9871592b99e2a3340edf76d6543f694180729b
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-16 19:00:24 +0200

    Create a login service that manages everything related to login

---
+++ HotelManagementSystem.Core/Application/Services/LoginService.cs
@@ -0,0 +1,55 @@
+﻿using HotelManagementSystem.Core.Application.Interfaces;
+using HotelManagementSystem.Core.Domain.Model;
+using HotelManagementSystem.Core.Domain.Repositories;
+
+namespace HotelManagementSystem.Core.Application.Services
+{
+    public class LoginService
+    {
+        private readonly IAuthTokenService _authTokenService;
+        private readonly IVerificationService _verificationService;
+        private readonly IGuestRepository _guestRepository;
+
+        public LoginService(IAuthTokenService authTokenService, IVerificationService verificationService, IGuestRepository guestRepository)
+        {
+            _authTokenService = authTokenService;
+            _verificationService = verificationService;
+            _guestRepository = guestRepository;
+        }
+
+        public Guest? SendVerificationCode(string phoneNr)
+        {
+            var guest = _guestRepository.GetGuestByPhoneNr(phoneNr);
+
+            if (guest == null)
+            {
+                return null;
+            }
+
+            _verificationService.Send(phoneNr);
+
+            return guest;
+        }
+
+        public string? VerifyVerificationCodeAndGenerateToken(Guid id, string verificationCode)
+        {
+            var guest = _guestRepository.GetById(id);
+
+            if (guest == null)
+            {
+                return null;
+            }
+
+            var verificationSucceed = _verificationService.Verify(guest.PhoneNr, verificationCode);
+
+            if (!verificationSucceed)
+            {
+                return null;
+            }
+
+            var token = _authTokenService.GenerateToken();
+
+            return token;
+        }
+    }
+}
