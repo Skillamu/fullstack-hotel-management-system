bd03f418230d65fd85d1689b4042fd63c823e38e
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-18 17:40:13 +0200

    Create a login controller that handles the process related to login

---
+++ HotelManagementSystem/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using HotelManagementSystem.Core.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManagementSystem.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly LoginService _loginService;
+
+        public LoginController(LoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost("guests")]
+        public ActionResult<Guid> SendVerificationCode([FromBody] string phoneNr)
+        {
+            var guestId = _loginService.SendVerificationCode(phoneNr);
+
+            return guestId == null ? BadRequest() : Ok(guestId);
+        }
+
+        [HttpPost("guests/{id}")]
+        public ActionResult<string> VerifyVerificationCodeAndGenerateToken(Guid id, [FromBody] string verificationCode)
+        {
+            var token = _loginService.VerifyVerificationCodeAndGenerateToken(id, verificationCode);
+
+            if (token == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(token);
+        }
+    }
+}
