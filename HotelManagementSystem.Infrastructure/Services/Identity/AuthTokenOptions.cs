ff11ae7347042c8c444e07bb13e2f21edbfda24d
Åmund Henriksen <ps4.c.aa@gmail.com>
2023-09-18 12:51:12 +0200

    Create AuthTokenOptions class to encapsulate configuration for the AuthTokenService class

---
+++ HotelManagementSystem.Infrastructure/Services/Identity/AuthTokenOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace HotelManagementSystem.Infrastructure.Services.Identity
+{
+    public class AuthTokenOptions
+    {
+        public const string Jwt = "jwt";
+
+        public string SecretKey { get; set; } = "";
+        public string Issuer { get; set; } = "";
+        public string Audience { get; set; } = "";
+    }
+}
