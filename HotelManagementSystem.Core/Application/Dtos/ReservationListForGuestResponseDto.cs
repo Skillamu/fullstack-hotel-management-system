d4b7cc45b114de75e4ec7672a2b59d56d8c5731a
Skillamu <ps4.c.aa@gmail.com>
2023-09-19 17:05:42 +0200

    Create a DTO that is used as a response when a guest requests its reservations

---
+++ HotelManagementSystem.Core/Application/Dtos/ReservationListForGuestResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelManagementSystem.Core.Application.Dtos
+{
+    public class ReservationListForGuestResponseDto
+    {
+        public short RoomNr { get; set; }
+        public string Type { get; set; }
+        public string CheckInDate { get; set; }
+        public string CheckOutDate { get; set; }
+    }
+}
