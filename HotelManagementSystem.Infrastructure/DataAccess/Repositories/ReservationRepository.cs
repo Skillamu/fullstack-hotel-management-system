35e3cd9c6e1b78180e54d7b7eabb50dacd6b370d
Skillamu <ps4.c.aa@gmail.com>
2023-09-20 11:35:21 +0200

    Implement method to get all reservations by guest id

--- HotelManagementSystem.Infrastructure/DataAccess/Repositories/ReservationRepository.cs
+++ HotelManagementSystem.Infrastructure/DataAccess/Repositories/ReservationRepository.cs
@@ -1,6 +1,6 @@
 ﻿using Dapper;
-using HotelManagementSystem.Core.Domain.Services;
 using HotelManagementSystem.Core.Domain.Model;
+using HotelManagementSystem.Core.Domain.Repositories;
 using HotelManagementSystem.Infrastructure.DataAccess.Factories;
 using HotelManagementSystem.Infrastructure.DataAccess.Mappers;
 using HotelManagementSystem.Infrastructure.DataAccess.Tables;
@@ -85,6 +85,32 @@ namespace HotelManagementSystem.Infrastructure.DataAccess.Repositories
             splitOn: "Id, Id, Id").SingleOrDefault();
 
             return reservation;
+        }
+
+        public IEnumerable<Reservation> GetAllByGuestId(Guid guestId)
+        {
+            var sql = @"SELECT reservation.id AS Id, check_in_date AS CheckInDate, check_out_date AS CheckOutDate,
+                        created_at_date AS CreatedAtdate, is_verified AS IsVerified,
+                        guest_id AS Id, first_name AS FirstName, last_name AS LastName, phone_nr AS PhoneNr,
+                        room_id AS Id, room_nr AS RoomNr, room_type_id AS Id, room_type.type AS Type,
+                        has_city_view AS HasCityView, has_bathtub AS HasBathtub
+                        FROM reservation
+                        JOIN guest ON reservation.guest_id = guest.id
+                        JOIN room ON reservation.room_id = room.id
+                        JOIN room_type ON room.room_type_id = room_type.id
+                        WHERE guest.id = @Id";
+
+            using var conn = _sqlConnectionFactory.CreateSqlConnection();
+
+            var reservations = conn.Query<ReservationTable, GuestTable, RoomTable, RoomTypeTable, Reservation>(
+                sql, (reservationTable, guestTable, roomTable, roomTypeTable) =>
+                {
+                    return ReservationMapper.ToDomain(reservationTable, guestTable, roomTable, roomTypeTable);
+                },
+            param: new { Id = guestId },
+            splitOn: "Id, Id, Id");
+
+            return reservations;
         }
 
         public void Update(Reservation reservation)
